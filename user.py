6915396d3fbf8ac14662b7c8fc720d52b383e5ca
Anthony <anthonygibson24@gmail.com>
2022-04-10 23:07:57 -0500

    Added MVC and sucessfully storing entered data to db

--- user.py
+++ user.py
@@ -1,5 +1,5 @@
 # import the function that will return an instance of a connection
-from mysqlconnection import connectToMySQL
+from flask_app.config.mysqlconnection import connectToMySQL
 
 def function(arg):
     from xml.dom.xmlbuilder import _DOMInputSourceCharacterStreamType
